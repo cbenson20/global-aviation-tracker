5fe0a769a5664d2971505a9fdd1d155b56a9314e
cbenson20 <cbenson2@academic.rrc.ca>
2025-11-03 09:53:07 -0600

    Add consistent validations and associations to Airline

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,8 +1,12 @@
-class Route < ApplicationRecord
-  belongs_to :airline
-  belongs_to :source_airport, class_name: "Airport"
-  belongs_to :dest_airport, class_name: "Airport"
+class Airline < ApplicationRecord
+  has_many :routes, dependent: :destroy
+  has_many :served_source_airports, through: :routes, source: :source_airport
+  has_many :served_dest_airports, through: :routes, source: :dest_airport
+  # distinct list of airports (either direction)
+  has_many :served_airports, -> { distinct }, through: :routes, source: :source_airport
 
-  validates :stops, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
-  validates :codeshare, length: { maximum: 10 }, allow_blank: true
+  validates :name, presence: true
+  validates :iata, length: { maximum: 3 }, allow_blank: true
+  validates :icao, length: { maximum: 4 }, allow_blank: true
+  validates :active, inclusion: { in: %w[Y N], message: "must be Y or N" }, allow_blank: true
 end
