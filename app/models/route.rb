7f5f7e799970b20463a8ec2431386d5df9557650
cbenson20 <cbenson2@academic.rrc.ca>
2025-11-03 09:53:34 -0600

    Add consistent validations and associations to Route model

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,7 +1,8 @@
 class Route < ApplicationRecord
   belongs_to :airline
   belongs_to :source_airport, class_name: "Airport"
-  belongs_to :dest_airport,   class_name: "Airport"
+  belongs_to :dest_airport, class_name: "Airport"
 
   validates :stops, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
+  validates :codeshare, length: { maximum: 10 }, allow_blank: true
 end
